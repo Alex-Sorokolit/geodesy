3f212662b0bd51063da76a181da4899da31f4c27
Alex-Sorokolit <sorokolital@gmail.com>
2023-08-09 18:42:34 +0300

    add margin

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Section from "../Section/Section";
+import css from "./Contacts.module.css";
+import { MdEmail, MdPhone } from "react-icons/md";
 
 const Contacts = () => {
   return (
     <Section title="Контакти">
-      <ul>
-        <li>
-          <a href="mailto:sorokolital@gmail.com">sorokolital@gmail.com</a>
+      <ul className={css.contactsList}>
+        <li className={css.contactsItem}>
+          <a href="mailto:sorokolital@gmail.com" className={css.contactsItem}>
+            <MdEmail className={css.contactsIcon} />
+            sorokolital@gmail.com
+          </a>
         </li>
         <li>
-          <a href="tel:+380980145051">+380980145051</a>
+          <a href="tel:+380980145051" className={css.contactsItem}>
+            <MdPhone className={css.contactsIcon} />
+            +380980145051
+          </a>
         </li>
       </ul>
     </Section>
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,11 +1,12 @@
 // Секція поділяє сторінку на різні типи контенту, логічні блоки інформації
 import React from "react";
 import Container from "../Contaiter/Container";
+import css from "./Section.module.css";
 
 const Section = ({ title, children }) => {
   return (
     <Container>
-      {title && <h2>{title}</h2>}
+      {title && <h2 className={css.sectionTitle}>{title}</h2>}
       {children}
     </Container>
   );

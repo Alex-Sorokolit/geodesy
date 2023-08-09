b395211f2fe3dddc2452aa78bfd7a831862ed853
Alex-Sorokolit <104554703+Alex-Sorokolit@users.noreply.github.com>
2023-08-09 15:28:00 +0300

    add bg image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Section from "../Section/Section";
+import css from "./Hero.module.css";
 const Hero = () => {
   return (
-    <Section>
-      <h1>Геодезичні вимірювання</h1>
-    </Section>
+    <div className={css.heroWrapper}>
+      <Section>
+        <h1 className={css.heroTitle}>Геодезичні вимірювання</h1>
+        <p className={css.heroDescription}>
+          Професійне виконання геодезичних вимірювань, ділянки, котловану,
+          будинку чи конструкції.
+        </p>
+        <p className={css.heroDescription}>
+          Професійне виконання геодезичних вимірювань, ділянки, котловану,
+          будинку чи конструкції.
+        </p>
+      </Section>
+    </div>
   );
 };
 
